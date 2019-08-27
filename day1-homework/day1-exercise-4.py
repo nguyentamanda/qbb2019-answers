c36ad3d0944fb38bc319b03066dfe02ff4dabf34
Amanda Nguyen <anguye65@jhu.edu>
2019-08-27 09:23:19 -0400

    more notes

--- day1-homework/day1-exercise-4.py
+++ day1-homework/day1-exercise-4.py
@@ -19,4 +19,12 @@ for line in f:
     if len(chromosome) < 10:
         chromosome.append(fields[2])
 print (chromosome)
-    
+
+#another way to do it
+#you would still need to filter this so it counts the 10th alignment, this one would count the 10th line 
+# for i. line in enumerate f:
+#if i >= 10:
+#break
+#line = line.rstrip("\n")
+#fields = line.split("\t")
+#print(fields[2])
