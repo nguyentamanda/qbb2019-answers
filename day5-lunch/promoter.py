3e8c28390adb68f7917fa82b1892e19392759e22
Amanda Nguyen <anguye65@jhu.edu>
2019-08-30 13:07:40 -0400

    ex2

---
+++ day5-lunch/promoter.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+"""
+Usage: ./promoter.py <c_tab_path> 
+"""
+
+import sys 
+import pandas as pd 
+
+
+df=pd.read_csv(open(sys.argv[1]), sep="\t")
+
+genes = df.loc[:,(("t_name","start", "end", "chr", "strand"))]
+
+for i, t in genes.iterrows(): 
+    if t.loc["strand"] == "+":
+        prota = int(t.loc["start"]) + 500
+        protb = int(t.loc["start"]) - 500
+        a = t.loc["chr"]
+        t_name = t.loc["t_name"]
+        print(a, prota, protb, t_name, sep = "\t")
+    
+    elif t.loc["strand"]=="-":
+        protc = int(t.loc["end"])+500
+        protd = int(t.loc["end"])-500
+        a = t.loc["chr"]
+        t_name = t.loc["t_name"]
+        print(a, protc, protd, t_name, sep = "\t")
+
