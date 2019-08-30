6ff4459f8adba3efd465d084368fcf80083f3135
Amanda Nguyen <anguye65@jhu.edu>
2019-08-30 14:37:35 -0400

    dataframe

---
+++ day5-lunch/regression.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+"""
+Usage ./regression.py <fpkms> <histone1> <histone2> <histone3>
+
+fpkms = all file 
+
+./regression.py ../results/stringtie/SRR072893/t_data.ctab H3K4me1.tab H3K4me3.tab H3K9me3.tab 
+"""
+
+import sys 
+import numpy as np 
+import pandas as pd 
+import statsmodels.api as sm 
+
+fpkm = sys.argv[1]
+his1 = sys.argv[2]
+his2 = sys.argv[3]
+his3 = sys.argv[4]
+
+fpkm_df = pd.read_csv(fpkm, sep="\t", index_col ="t_name")
+his1_df = pd.read_csv(his1, sep="\t", index_col=0, header= None)
+his2_df = pd.read_csv(his2, sep="\t", index_col=0, header= None)
+his3_df = pd.read_csv(his3, sep="\t", index_col=0, header= None)
+
+hist_dict={"FPKM": fpkm_df.loc[:,"FPKM"], 
+            "H1": his1_df.iloc[:,-1],
+            "H2": his2_df.iloc[:,-1], 
+            "H3": his3_df.iloc[:,-1]}
+
+
