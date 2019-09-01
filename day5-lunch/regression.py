552ce961297dfb16cc6e14f688cd97b173e4e3c9
Amanda Nguyen <anguye65@jhu.edu>
2019-09-01 17:19:10 -0400

    ex4

--- day5-lunch/regression.py
+++ day5-lunch/regression.py
@@ -19,7 +19,7 @@ his2 = sys.argv[3]
 his3 = sys.argv[4]
 
 fpkm_df = pd.read_csv(fpkm, sep="\t", index_col ="t_name")
-his1_df = pd.read_csv(his1, sep="\t", index_col=0, header= None)
+his1_df = pd.read_csv(his1, sep="\t", index_col=0, header= None) #these files dont have a header
 his2_df = pd.read_csv(his2, sep="\t", index_col=0, header= None)
 his3_df = pd.read_csv(his3, sep="\t", index_col=0, header= None)
 
@@ -28,4 +28,8 @@ hist_dict={"FPKM": fpkm_df.loc[:,"FPKM"],
             "H2": his2_df.iloc[:,-1], 
             "H3": his3_df.iloc[:,-1]}
 
+histone_df = pd.DataFrame(hist_dict )
 
+model = sm.formula.ols(formula = "FPKM ~ H1 + H2 + H3", data=histone_df)
+ols_result = model.fit()
+print(ols_result.summary())
